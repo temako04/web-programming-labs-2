e63e51017605ac97f5fbd54418518d6b636f12e2
Артём Конкин <artemkonkin04@yandex.ru>
2024-09-07 14:10:57 +0700

    Правки коммитов

--- app.py
+++ app.py
@@ -2,5 +2,49 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <nav>
+            <ul>
+                <li><a href="/lab1">Первая лабораторная</a></li>
+            </ul>
+        </nav>
+
+        <footer>
+            &copy; Конкин Артём, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</h1>
+
+        <footer>
+            &copy; Конкин Артём, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
