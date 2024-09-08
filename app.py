8702ef3b7060ac0a86d838a3d14610bb09234596
Artem Konkin <artemkonkin04@yandex.ru>
2024-09-08 21:20:21 +0700

    Роуты

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def start():
     
 @app.route("/menu")
 def menu():
-    return """
+    return '''
 <!doctype html>
 <html>
     <head>
@@ -28,15 +28,16 @@ def menu():
         </footer>
     </body>
 </html>
-"""
+'''
 
 @app.route("/lab1")
 def lab1():
-    return """
+    return '''
 <!doctype html>
 <html>
     <head>
         <title>Лабораторная работа 1</title>
+        <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
     </head>
     <body>
         <header>
@@ -44,13 +45,21 @@ def lab1():
         </header>
 
         <h1>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</h1>
+        <a href="/menu">Меню</a>
+        <h2>Реализованные роуты</h2>
+        <ul>
+            <li><a href="/lab1/oak">/lab1/oak - Дуб </a></li>
+            <li><a href="/lab1/student">/lab1/student - Студент </a></li>
+            <li><a href="/lab1/python">/lab1/python - Пайтон </a></li>
+            <li><a href="/lab1/camry">/lab1/camry - Камри </a></li>
+        </ul>
 
         <footer>
             &copy; Конкин Артём, ФБИ-23, 3 курс, 2024
         </footer>
     </body>
 </html>
-"""
+'''
 
 @app.route('/lab1/oak')
 def oak():
@@ -65,3 +74,46 @@ def oak():
 </html>
 '''
 
+@app.route('/lab1/student')
+def student():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Конкин Артём Константинович</h1>
+        <img src="''' + url_for('static', filename='ngtu.jpg') + '''">
+    </body>
+</html>
+'''
+
+@app.route('/lab1/python')
+def python():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Python — это высокоуровневый язык программирования, отличающийся эффективностью, простотой и универсальностью использования.</h1>
+        <h1>Он широко применяется в разработке веб-приложений и прикладного программного обеспечения, а также в машинном обучении и обработке больших данных.</h1>
+        <h1>За счет простого и интуитивно понятного синтаксиса является одним из распространенных языков для обучения программированию.</h1>
+        <img src="''' + url_for('static', filename='python.png') + '''">
+    </body>
+</html>
+'''
+
+@app.route('/lab1/camry')
+def camry():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Toyota Camry — это один из самых популярных среднеразмерных седанов, производимых японским автопроизводителем Toyota.</h1>
+        <h1>С момента своего запуска в 1982 году, Camry завоевала признание за надежность, долговечность и высокий уровень комфорта.</h1>
+        <h1>Модель отличается элегантным дизайном и современными технологиями, что делает её привлекательным выбором как для
+        городских поездок, так и для длительных путешествий.</h1>
+        <img src="''' + url_for('static', filename='camry.jpg') + '''">
+    </body>
+</html>
+'''
