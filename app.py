2595516e89bc21e58caeb8e3fdeebb347d7b0651
Артём Конкин <artemkonkin04@yandex.ru>
2024-09-21 12:30:49 +0700

    Имя студента

--- app.py
+++ app.py
@@ -119,4 +119,5 @@ def camry():
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Конкин Артём'
+    return render_template('example.html', name=name)
