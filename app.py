15175eca79946352dad554215010d6f18bd3d8cc
Artem Konkin <artemkonkin04@yandex.ru>
2024-09-29 21:34:35 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def a():
 def a1():
     return 'ok'
 
-flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -134,6 +134,21 @@ def flowers(flower_id):
         return 'Такого цветка нет', 404
     else:
         return 'Цветок: ' + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Добавлен новый цветок</h1>
+    <p>Название нового цветка: {name} </p>
+    <p>Всего цветов: {len(flower_list)}</p>
+    <p>Полный список: {flower_list}</p>
+    </body>
+</html> 
+'''
 
 @app.route('/lab2/example')
 def example():
