c83fd3750e0e4dae01602a262e3c2730d636cf1f
Артём Конкин <artemkonkin04@yandex.ru>
2024-10-05 13:09:34 +0700

    Заготовка к 3-ей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 
 @app.route("/")
@@ -32,6 +34,9 @@ def menu():
             
             <ol>
                 <li><a href="/lab2">Вторая лабораторная</a></li>
+            </ol>
+            <ol>
+                <li><a href="/lab3">Третья лабораторная</a></li>
             </ol>
 
         <footer>
---
+++ lab3.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, redirect, url_for, render_template
+lab3 = Blueprint('lab3', __name__)
+
+
+@lab3.route('/lab3/')
+def lab():
+    return render_template('lab3/lab3.html')
+
