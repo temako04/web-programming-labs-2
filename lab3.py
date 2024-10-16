34f5eb50cfb167223300f9e1321d95d1189e63a5
Artem Konkin <artemkonkin04@yandex.ru>
2024-10-17 00:09:47 +0700

    Билет на поезд + очистка всех куки

--- lab3.py
+++ lab3.py
@@ -108,3 +108,76 @@ def settings():
         fontweight = request.cookies.get('fontweight')
         resp = make_response(render_template('lab3/settings.html', color=color, backgroundcolor=backgroundcolor, fontsize=fontsize, fontweight=fontweight))
     return resp
+
+
+@lab3.route('/lab3/form2/')
+def form2():
+    errors = {}
+    pass_name = request.args.get('pass_name')
+    if pass_name == '':
+        errors['pass_name'] = 'Заполните поле!'
+
+    shelf = request.args.get('shelf')
+    bedding = request.args.get('bedding') == 'on'
+    luggage = request.args.get('luggage') == 'on'
+    
+    age = request.args.get('age')
+    if age == None:
+        errors['age'] = ''
+    elif age =='':
+        errors['age'] = 'Заполните поле!'
+    else:
+        age = int(age)
+        if age < 1 or age > 120:
+            errors['age'] = 'Возраст должен быть от 1 до 120 лет!'
+
+    departure = request.args.get('departure')
+    if departure == '':
+        errors['departure'] = 'Заполните поле!'
+
+    destination = request.args.get('destination')
+    if destination == '':
+        errors['destination'] = 'Заполните поле!'
+
+    date = request.args.get('date')
+    if date == '':
+        errors['date'] = 'Заполните поле!'
+    
+    insurance = request.args.get('insurance') == 'on'
+
+    if 'age' in errors:
+        price = 0
+        ticket_type = ''
+    else:
+        if age >= 18:
+            base_price = 1000
+            ticket_type = 'Взрослый'
+        else:
+            base_price = 700
+            ticket_type = 'Детский'
+
+        if shelf in ['lower', 'lower_side']:
+            base_price += 100
+        if bedding:
+            base_price += 75
+        if luggage:
+            base_price += 250
+        if insurance:
+            base_price += 150
+
+        price = base_price
+
+    return render_template('lab3/form2.html', errors=errors, pass_name=pass_name, shelf=shelf,
+                           bedding=bedding, luggage=luggage, age=age, departure=departure,
+                           destination=destination, date=date, insurance=insurance,
+                           ticket_type=ticket_type, price=price)
+
+
+@lab3.route('/lab3/del_cookie_2')
+def del_cookie_2():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('background-color')
+    resp.delete_cookie('fontsize')
+    resp.delete_cookie('fontweight')
+    return resp
