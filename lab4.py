da8b969d7bc6a860a2b1a41006925b8061b8ce8c
Artem Konkin <artemkonkin04@yandex.ru>
2024-11-01 16:40:18 +0700

    Cписок пользователей

--- lab4.py
+++ lab4.py
@@ -99,6 +99,13 @@ def tree():
 
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'jerry', 'password': '333'},
+    {'login': 'tom', 'password': '777'},
+]
+
 @lab4.route('/lab4/login', methods = ['GET','POST'])
 def login():
     if request.method == 'GET':
@@ -107,8 +114,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
     
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
