738f964447ca42077513bd14ad3d3740a6add9b6
Artem Konkin <artemkonkin04@yandex.ru>
2024-11-13 22:25:14 +0700

    Хеширование паролей

--- lab5.py
+++ lab5.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, url_for, render_template, request, make_response, redirect, session
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
 lab5 = Blueprint('lab5', __name__)
 
 
@@ -11,7 +12,7 @@ def lab():
 def db_connect():
     conn= psycopg2.connect(
         host = '127.0.0.1',
-        database = 'kb',
+        database = 'artem_konkin_knowledge_base',
         user = 'artem_konkin_knowledge_base',
         password = '123'
     )
@@ -43,7 +44,8 @@ def register():
         return render_template('lab5/register.html',
                                 error="Такой пользователь уже существует")
 
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    password_hash = generate_password_hash(password)
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
 
     db_close(conn,cur)
     return render_template('lab5/success.html', login=login)
@@ -73,7 +75,7 @@ def login():
         return render_template('lab5/login.html',
                                 error='Логин и/или пароль неверны')
 
-    if user['password'] != password:
+    if not check_password_hash(user['password'], password):
         db_close(conn,cur)
         return render_template('lab5/login.html',
                                 error='Логин и/или пароль неверны')
