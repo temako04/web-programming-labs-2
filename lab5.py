5f3f52fa5585c081196d3a35ab29cc922d72654e
Artem Konkin <artemkonkin04@yandex.ru>
2024-11-13 21:01:54 +0700

    Авторизация

--- lab5.py
+++ lab5.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, redirect, url_for, render_template, request, make_response, redirect, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 lab5 = Blueprint('lab5', __name__)
 
 
-@lab5.route('/lab5/shablon')
-def shablon():
-    return render_template('/lab5/shablon.html')
+@lab5.route('/lab5/lab5')
+def lab5():
+    return render_template('/lab5/lab5.html', login=session.get('login'))
 
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
@@ -42,3 +43,47 @@ def register():
 @lab5.route('/lab5/success')
 def success():
     return render_template('/lab5/success.html')
+
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/login.html', error='Заполните поля')
+
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'artem_konkin_knowledge_base',
+        user = 'artem_konkin_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',
+                                error='Логин и/или пароль неверны')
+
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',
+                                error='Логин и/или пароль неверны')
+
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html',login=login)
+
+@lab5.route('/lab5/success_login')
+def success_login():
+    return render_template('/lab5/success_login.html')
+
