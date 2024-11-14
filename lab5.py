980ad94755865d2367e45b275c69a5209774a162
Artem Konkin <artemkonkin04@yandex.ru>
2024-11-15 00:09:34 +0700

    Правочки

--- lab5.py
+++ lab5.py
@@ -134,7 +134,7 @@ def create():
         cur.execute("INSERT INTO articles(login_id, title, article_text) \
             VALUES (%s, %s, %s);", (login_id, title, article_text))
     else:
-        cur.execute("INSERT INTO articles(login_id, title, aritcle_text) \
+        cur.execute("INSERT INTO articles(login_id, title, article_text) \
             VALUES (?, ?, ?);", (login_id, title, article_text))
 
     db_close(conn,cur)
