de2c5b982e9a7e478e8c33e23d399a1b5b21b910
Artem Konkin <artemkonkin04@yandex.ru>
2024-11-14 17:04:40 +0700

    Исправление SQL-иньекции

--- lab5.py
+++ lab5.py
@@ -38,14 +38,14 @@ def register():
 
     conn,cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", {login})
     if cur.fetchone():
         db_close(conn,cur)
         return render_template('lab5/register.html',
                                 error="Такой пользователь уже существует")
 
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s;", (login, password_hash))
 
     db_close(conn,cur)
     return render_template('lab5/success.html', login=login)
@@ -67,7 +67,7 @@ def login():
 
     conn,cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -105,8 +105,8 @@ def create():
     cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     login_id = cur.fetchone()["id"]
 
-    cur.execute(f"INSERT INTO articles(login_id, title, article_text) \
-                VALUES ({login_id}, '{title}', '{article_text}');")
+    cur.execute("INSERT INTO articles(login_id, title, article_text) \
+                VALUES (%s, %s, %s);", (login_id, title, article_text))
 
     db_close(conn,cur)
     return redirect('/lab5/lab5')
@@ -119,10 +119,10 @@ def list():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    cur.execute("SELECT id FROM users WHERE login=%s;", (login, ))
     login_id = cur.fetchone()["id"]
 
-    cur.execute(f"SELECT * FROM articles WHERE login_id='{login_id}';")
+    cur.execute("SELECT * FROM articles WHERE login_id=%s;", (login_id, ))
     articles = cur.fetchall()
 
     db_close(conn, cur)
