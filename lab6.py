e55d5f25cfe4b7f3165036424c9788abce54c68f
Artem Konkin <artemkonkin04@yandex.ru>
2024-11-16 22:44:54 +0700

    правка3

--- lab6.py
+++ lab6.py
@@ -4,6 +4,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
+from os import path
 from dotenv import load_dotenv
 
 lab6 = Blueprint('lab6', __name__)
